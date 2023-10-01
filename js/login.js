37572db1123d1d1dc1d5c6891d7815c8727c2549
Sunny-Rubayet <sunnyrubayet20@gmail.com>
2023-10-01 21:21:13 +0600

    input filed addevent done

--- js/login.js
+++ js/login.js
@@ -1,3 +1,20 @@
+// Step:01: Add to addEventListener for the submit button
+
+
+
 document.getElementById('btn-submit').addEventListener('click',function(){
-    console.log("login bangladesh")
+
+    // get email address input field area
+    const emailField=document.getElementById('email-field');
+    const email=emailField.value;
+    const passWordField=document.getElementById('password-field');
+    const password=passWordField.value;
+    if(email==='sunny@sunny.com' && password==='1234'){
+        console.log('valid user')
+    }
+
+    else{
+        console.log('invalid user')
+    }
+
 })
